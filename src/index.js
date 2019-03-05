e51343ab7e1bf40e5efb1a8d00b7f00b00b4711b
Artem Lipchik <tommerg09@gmail.com>
2019-03-05 13:38:11 +0300

    I_ve calculated how many zeros in the end of number, which is factorial of number in some base base system

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  var bases = [];
+
+      for (i=2; i < base;){
+          if(base % i == 0){
+            bases.push(i);
+            base /= i;
+            }
+          else {
+            i++;
+            }
+        }
+      
+      bases.push(base);
+
+      var solve = Infinity;
+
+      while(bases.length > 0) {
+        var x = bases[0];
+        var y = bases.filter(item => item === x).length;
+        
+        var numberR = number;
+        var zeros = 0;
+        while (Math.floor(numberR) > 0) {
+          numberR /= x;
+          zeros += Math.floor(numberR);
+        }
+
+        zeros /= y;
+
+        if (zeros < solve) {
+          solve = Math.floor(zeros);
+        }
+
+        bases = bases.filter(element => element !== x);
+
+      }
+     return solve;
+    }
